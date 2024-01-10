15cafdea16ac9f8cc57752fb1b7bdb55a838fcc8
Liulun10 <148021967+Liulun10@users.noreply.github.com>
2024-01-11 02:32:30 +0800

    Update Email.py

--- Email.py
+++ Email.py
@@ -2,9 +2,9 @@ import smtplib
 smtp=smtplib.SMTP('smtp.gmail.com', 587)
 smtp.ehlo()
 smtp.starttls()
-smtp.login('c2735940449@gmail.com','ygch szmk oeyt lzwe')
-from_addr='c2735940449@gmail.com'
-to_addr="s110213060@mail1.ncnu.edu.tw"
+smtp.login('yourmail','password')
+from_addr='yourmail'
+to_addr="mailto"
 msg="Subject:Gmail sent by Python scripts\n no anger!"
 status=smtp.sendmail(from_addr, to_addr, msg)#加密文件，避免私密信息被截取
 if status=={}:
